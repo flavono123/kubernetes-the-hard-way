05e7d41ceab9f2fe5a53905f34df206324ac65cb
flavono123 <flavono123@gmail.com>
2022-05-03 11:10:31 +0900

    Add Ansible custom filters from/to_toml
    Signed-off-by: flavono123 <flavono123@gmail.com>

---
+++ provisioning/filters/toml.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import toml
+import json
+
+class FilterModule(object):
+
+    def filters(self):
+        return {'from_toml': self.from_toml, 'to_toml': self.to_toml}
+
+
+    def from_toml(self, toml_string):
+        return toml.loads(toml_string)
+
+
+    def to_toml(self, yaml_variable):
+        s = json.dumps(dict(yaml_variable))
+        d = json.loads(s)
+        return toml.dumps(d)
+    #return toml.dumps(dict(variable))
